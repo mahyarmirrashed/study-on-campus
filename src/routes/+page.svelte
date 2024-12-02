24de738de5fbba5ead1b3204400cac047985b936
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-12-02 16:45:34 -0600

    chore: add button on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+
+<Button>Click me</Button>
