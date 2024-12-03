b587e8e6f7c43f1fbc7e638e14aee2532a4e7fe0
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-12-03 17:13:53 -0600

    feat: add maptiler maps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
-  import { MapLibre } from "svelte-maplibre";
+  import { PUBLIC_MAPTILER_KEY } from "$env/static/public";
+  import { MapLibre, FillExtrusionLayer } from "svelte-maplibre";
+
+  const maptilerStyle = `https://api.maptiler.com/maps/streets-v2/style.json?key=${PUBLIC_MAPTILER_KEY}`;
 </script>
 
-<MapLibre
-  style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
-  class="relative aspect-[9/16] max-h-[70vh] w-full sm:aspect-video sm:max-h-full"
-  standardControls
-/>
+<MapLibre style={maptilerStyle} class="min-h-screen" standardControls>
+  <FillExtrusionLayer
+    source="maptiler_planet"
+    sourceLayer="building"
+    paint={{}}
+  />
+</MapLibre>
