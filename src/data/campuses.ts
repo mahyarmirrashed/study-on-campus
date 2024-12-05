216648939c1119ce35a00a43d19f519d14d3c05d
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-12-05 01:27:43 -0600

    feat: add locations directly into campus type

--- src/data/campuses.ts
+++ src/data/campuses.ts
@@ -1,14 +1,18 @@
 import { type Campus } from "$src/campuses";
+import { locations as universityOfManitobaLocations } from "./en/university-of-manitoba";
+import { locations as universityOfWaterlooLocations } from "./en/university-of-waterloo";
 
 export const campuses: Campus[] = [
   {
     value: "university-of-manitoba",
     label: "University of Manitoba",
     location: { lat: 49.808, lng: -97.134 },
+    spaces: universityOfManitobaLocations,
   },
   {
     value: "university-of-waterloo",
     label: "University of Waterloo",
     location: { lat: 43.469, lng: -80.542 },
+    spaces: universityOfWaterlooLocations,
   },
 ];
