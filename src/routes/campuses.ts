77cd79a578b809f121dbbe7056752135e9fe1daa
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-12-04 23:41:54 -0600

    chore: tune location of campuses

--- src/routes/campuses.ts
+++ src/routes/campuses.ts
@@ -8,11 +8,11 @@ export const campuses: Campus[] = [
   {
     value: "university-of-manitoba",
     label: "University of Manitoba",
-    location: { lat: 49.810556, lng: -97.13527 },
+    location: { lat: 49.808, lng: -97.134 },
   },
   {
     value: "university-of-waterloo",
     label: "University of Waterloo",
-    location: { lat: 43.47233, lng: -80.54486 },
+    location: { lat: 43.469, lng: -80.542 },
   },
 ];
