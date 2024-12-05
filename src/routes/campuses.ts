8d3960921aa27d588b1701f43b817a88f75c2d2a
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-12-04 23:43:01 -0600

    feat: add metadata for campus display on map

--- src/routes/campuses.ts
+++ src/routes/campuses.ts
@@ -2,6 +2,13 @@ export type Campus = {
   value: string;
   label: string;
   location: { lat: number; lng: number };
+  metadata?: {
+    display?: {
+      zoom?: number;
+      pitch?: number;
+      bearing?: number;
+    };
+  };
 };
 
 export const campuses: Campus[] = [
