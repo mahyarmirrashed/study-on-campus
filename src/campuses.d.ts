13af48a9fdaa40bbe7ebf7bad4032c22503ece07
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-12-05 01:27:31 -0600

    feat: modify campus type to include spaces

--- src/campuses.d.ts
+++ src/campuses.d.ts
@@ -1,7 +1,10 @@
+import type { Space } from "./spaces";
+
 export type Campus = {
   value: string;
   label: string;
   location: { lat: number; lng: number };
+  spaces: Space[];
   metadata?: {
     display?: {
       zoom?: number;
