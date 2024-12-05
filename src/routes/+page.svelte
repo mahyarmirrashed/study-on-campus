2a441f761f5c79eab919de0583655a64abdf3455
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-12-04 18:58:32 -0600

    feat: add combobox for selecting campus

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
   import { PUBLIC_MAPTILER_KEY } from "$env/static/public";
   import { Button } from "$lib/components/ui/button/index.js";
+  import * as Command from "$lib/components/ui/command/index.js";
+  import * as Popover from "$lib/components/ui/popover/index.js";
+  import { cn } from "$lib/utils.js";
 
-  import { Sun, Moon } from "lucide-svelte/icons";
+  import { Check, ChevronsUpDown, Sun, Moon } from "lucide-svelte/icons";
   import { toggleMode, mode } from "mode-watcher";
+  import { tick } from "svelte";
   import { MapLibre } from "svelte-maplibre";
+
+  import { campuses } from "./campuses";
+
+  let open = $state(false);
+  let value = $state("");
+  let triggerRef = $state<HTMLButtonElement>(null!);
+  const selectedCampus = $derived(
+    campuses.find((f) => f.value === value)?.label,
+  );
 
   const maptilerTopoLightStyle = `https://api.maptiler.com/maps/topo-v2/style.json?key=${PUBLIC_MAPTILER_KEY}`;
   const maptilerTopoDarkStyle = `https://api.maptiler.com/maps/topo-v2-dark/style.json?key=${PUBLIC_MAPTILER_KEY}`;
@@ -12,9 +25,62 @@
   const maptilerStyle = $derived(
     $mode === "dark" ? maptilerTopoDarkStyle : maptilerTopoLightStyle,
   );
+
+  function closeAndFocusTrigger() {
+    open = false;
+    tick().then(() => {
+      triggerRef.focus();
+    });
+  }
 </script>
 
 <MapLibre style={maptilerStyle} class="min-h-screen" standardControls />
+
+<div class="z-10 absolute top-2.5 left-1/2 -translate-x-1/2">
+  <Popover.Root bind:open>
+    <Popover.Trigger bind:ref={triggerRef}>
+      {#snippet child({ props })}
+        <Button
+          variant="outline"
+          class="w-[200px] justify-between"
+          {...props}
+          role="combobox"
+          aria-expanded={open}
+        >
+          {selectedCampus || "Select a campus..."}
+          <ChevronsUpDown class="opacity-50" />
+        </Button>
+      {/snippet}
+    </Popover.Trigger>
+    <Popover.Content class="w-[200px] p-0">
+      <Command.Root>
+        <Command.Input placeholder="Search framework..." />
+        <Command.List>
+          <Command.Empty>No campus found.</Command.Empty>
+          <Command.Group>
+            {#each campuses as campus}
+              <Command.Item
+                value={campus.value}
+                onSelect={() => {
+                  value = campus.value;
+                  closeAndFocusTrigger();
+                }}
+              >
+                <Check
+                  class={cn(
+                    "ml-auto",
+                    value !== campus.value && "text-transparent",
+                  )}
+                />
+                {campus.label}
+              </Command.Item>
+            {/each}
+          </Command.Group>
+        </Command.List>
+      </Command.Root>
+    </Popover.Content>
+  </Popover.Root>
+</div>
 
 <Button
   class="absolute top-2.5 right-2.5 z-10"
---
+++ src/routes/campuses.ts
@@ -0,0 +1,12 @@
+export const campuses = [
+  {
+    value: "university-of-manitoba",
+    label: "University of Manitoba",
+    location: { lat: 49.810556, lng: -97.13527 },
+  },
+  {
+    value: "university-of-waterloo",
+    label: "University of Waterloo",
+    location: { lat: 43.47233, lng: -80.54486 },
+  },
+];
