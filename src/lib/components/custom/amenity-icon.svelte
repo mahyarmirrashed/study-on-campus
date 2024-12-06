ddb25ebb3d002b01ec1b59ea61454a31c0ebb8e5
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-12-05 23:20:41 -0600

    feat: add amenity icon component

---
+++ src/lib/components/custom/amenity-icon.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import type { SpaceAmenities } from "$src/spaces.d";
+  import {
+    Computer,
+    Plug,
+    Printer,
+    Box,
+    Droplet,
+    Wifi,
+    CornerDownRight,
+    Icon,
+  } from "lucide-svelte/icons";
+
+  export let amenity: SpaceAmenities;
+
+  const amenityMap: Record<SpaceAmenities, typeof Icon> = {
+    Computers: Computer,
+    Outlets: Plug,
+    Printing: Printer,
+    Rooms: Box,
+    "Water Fountains": Droplet,
+    WiFi: Wifi,
+  };
+</script>
+
+<svelte:component
+  this={amenityMap[amenity] ?? CornerDownRight}
+  class="h-fit w-fit"
+/>
