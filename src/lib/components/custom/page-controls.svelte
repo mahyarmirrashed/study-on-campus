4110279b6d03075ac247fdff570160864c9ef9e5
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-12-06 22:18:43 -0600

    feat: add ability to switch languages

--- src/lib/components/custom/page-controls.svelte
+++ src/lib/components/custom/page-controls.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
   import { Button } from "$lib/components/ui/button/index.js";
+  import {
+    setLanguageTag,
+    type AvailableLanguageTag,
+  } from "$src/lib/paraglide/runtime";
   import { Sun, Moon } from "lucide-svelte/icons";
   import { toggleMode } from "mode-watcher";
+  import { PersistedState } from "runed";
+
+  const language = new PersistedState<AvailableLanguageTag>("language", "fr");
+
+  function switchLanguage() {
+    if (language.current === "en") language.current = "fr";
+    else if (language.current === "fr") language.current = "en";
+
+    setLanguageTag(language.current);
+  }
 </script>
 
-<div class="absolute right-2.5 top-2.5 z-10">
+<div class="absolute right-2.5 top-2.5 z-10 flex flex-col gap-y-2">
   <Button onclick={toggleMode} variant="outline" size="icon">
     <Sun
       class="h-fit w-fit rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
@@ -13,5 +27,14 @@
       class="absolute h-fit w-fit rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
     />
     <span class="sr-only">Toggle theme</span>
+  </Button>
+  <Button
+    onclick={switchLanguage}
+    variant="outline"
+    size="icon"
+    class="uppercase"
+  >
+    {language.current}
+    <span class="sr-only">Toggle language</span>
   </Button>
 </div>
