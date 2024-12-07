58f07f60b818eb36a273348dfa9376770e51fed0
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-12-07 00:22:29 -0600

    feat: add google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,21 @@
   let { children } = $props();
 </script>
 
+<svelte:head>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-SK8KKXP57Z">
+  </script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+
+    gtag("config", "G-SK8KKXP57Z");
+  </script>
+</svelte:head>
+
 <ModeWatcher />
 <ParaglideJS {i18n}>
   {@render children()}
